9626a1d8fee8704239964bb5627e2e65c14e93a2
Natthapong <65070079@kmitl.ac.th>
2024-10-11 15:40:05 +0700

    Update

--- eldercare/eldercareapp/urls.py
+++ eldercare/eldercareapp/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = 'eldercare'  # สำคัญมาก! เพื่อให้สามารถเรียกใช้ URL ได้ถูกต้อง
 
 urlpatterns = [
-    path('create_appointment/', views.create_appointment, name='create_appointment'),
-    path('appointment_list/', views.appointment_list, name='appointment_list'),
-    path('caregiver/<int:caregiver_id>/', views.caregiver_profile, name='caregiver_profile'),
-    path('elder/<int:elder_id>/', views.elder_profile, name='elder_profile'),
+    # path('create_appointment/', views.create_appointment, name='create_appointment'),
+    # path('appointment_list/', views.appointment_list, name='appointment_list'),
+    # path('caregiver/<int:caregiver_id>/', views.caregiver_profile, name='caregiver_profile'),
+    # path('elder/<int:elder_id>/', views.elder_profile, name='elder_profile'),
     # ... other URLs for your eldercare app
 ]
