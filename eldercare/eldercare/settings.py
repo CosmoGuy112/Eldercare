8fdc8e800f5231a799a707aa319ee9df13605a61
CosmoGuy112 <65070079@kmitl.ac.th>
2024-10-14 13:32:19 +0000

    fix setting

--- eldercare/eldercare/settings.py
+++ eldercare/eldercare/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_user'),
+        'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),  # ชื่อ service ของฐานข้อมูลจาก docker-compose.yml
         'PORT': os.environ.get('POSTGRES_PORT'),
