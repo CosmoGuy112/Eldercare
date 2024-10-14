1e3497b58329ea08629d3b2f18f0fcd838bcc73a
Bike <Bikeza7575@gmail.com>
2024-10-14 15:34:44 +0700

    login picelder

--- eldercare/eldercareapp/urls.py
+++ eldercare/eldercareapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     
     path('create_elder_profile/', views.CreateElderProfileView.as_view(), name='create_elder_profile'),  # หน้าเพิ่มโปรไฟล์ Elder
     path('listelder/', views.ListElderView.as_view(), name='listelder'),  # หน้าแสดงรายการ Elder
+    path('elderdetail/<int:pk>/', views.Elderdetail.as_view(), name='elderdetail'),  # หน้าแสดงรายการ Elder
 
     # เพิ่ม URL สำหรับ Caregiver Detail และ Booking
     path('caregiver/<int:pk>/', views.CaregiverDetailView.as_view(), name='caregiver_detail'),  # URL สำหรับดูรายละเอียด
